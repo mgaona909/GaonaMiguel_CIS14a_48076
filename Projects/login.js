a2d804622c9e678ee4e4c4e1f1c0148675fed221
mgaona909 <mgaona909@gmail.com>
2014-11-26 10:59:40 -0800

    Signed-off-by: mgaona909 <mgaona909@gmail.com>

--- Projects/login.js
+++ Projects/login.js
@@ -1,44 +1,11 @@
-// JavaScript Document
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
-function validate(){
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
-	if ( username == "Formget" && password == "formget#123"){
-		alert ("Login successfully");
-		window.location = "success.html"; // Redirecting to other page.
-		return false;
+function pasuser(form) {
+if (form.id.value=="JavaScript") { 
+	if (form.pass.value=="Kit") {              
+		location="page2.html" 
+	} else {
+		alert("Invalid Password")
 	}
-	else{
-		attempt --;// Decrementing by one.
-		alert("You have left "+attempt+" attempt;");
-		// Disabling fields after 3 attempts.
-		if( attempt == 0){
-			document.getElementById("username").disabled = true;	
-			document.getElementById("password").disabled = true;
-			document.getElementById("submit").disabled = true;
-			return false;
-		}
-	}
-}
-
-
-
-function setLogin(){
-	//First we set the cookie date to a year hence.
-	var now= new Date();
-	now.setTime(now.getTime() + 365 * 24 * 60 * 60 * 1000);
-	//We call the function GetCookie() with the name, "username".
-	var username = GetCookie("username");
-	if ((!username)||(username=='null')) { 
-		username = prompt("Please enter your name:", "");
-	}
-	setCookie("username", username, now);
-	if (username) {
-		document.write(username);
-		setCookie("username", username, now);
-	}
-	else{
-	document.write("You didn\'t enter your name.<a href=# onClick=\"MyNamer();history.go(-1)\">Enter it now?</a>");
+	} else {  
+		alert("Invalid UserID")
 	}
 }
