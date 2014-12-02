f0eabc75b1d77e7f8e26b729f956e921e12f49fb
Miguel Gaona <mgaona909@gmail.com>
2014-12-02 15:39:05 -0800

    Signed-off-by: Miguel Gaona <mgaona909@gmail.com>

---
+++ Projects/Business Site V2/scripts/shoppingCart.js
@@ -0,0 +1,42 @@
+var shoppingCart = {
+    total:0,
+    lastTransactionAmount: 0,
+    
+	add: function(itemCost){
+        this.total += itemCost;
+        this.lastTransactionAmount = itemCost;
+    },
+   
+    scan: function(item){
+        switch (item){
+        case "tab2": 
+			this.add(320);
+			break;
+        case "victory": 
+			this.add(100); 
+			break;
+        case "pavilion": 
+			this.add(410); 
+			break;
+        case "m7": 
+			this.add(300); 
+			break;
+		case "lenovo": 
+			this.add(485); 
+			break;
+		case "moto": 
+			this.add(250); 
+			break;
+		case "ps3": 
+			this.add(250); 
+			break;
+        }
+		alert("Product has been added to your Cart! Your total so far is: $"+this.total);
+		sessionStorage.setItem('Total', JSON.stringify(shoppingCart.total));	
+    },
+   
+    voidLastTransaction: function(){
+        this.total -= this.lastTransactionAmount;
+        this.lastTransactionAmount = 0;
+    },
+};
